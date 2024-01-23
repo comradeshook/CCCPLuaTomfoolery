bd767bc6a38935ee722eb4871991c21f532446d6
comradeshook <comradeshook@gmail.com>
2024-01-23 13:38:48 +0100

    Added more functions, mostly getters

--- TableVectorLibrary.lua
+++ TableVectorLibrary.lua
@@ -1,4 +1,5 @@
 local Rad = math.rad;
+local Deg = math.deg;
 local Max = math.max;
 local Min = math.min;
 local Cos = math.cos;
@@ -8,6 +9,9 @@ local Ceil = math.ceil;
 local Random = math.random;
 local Atan2 = math.atan2;
 local Pi = math.pi;
+local Floor = math.floor;
+local Ceil = math.ceil;
+local Abs = math.abs;
 
 -- TO USE: First, you may need to convert an actual vector to a table via VecToTable().
 -- After that, you can use all the following operators to essentially treat
@@ -21,6 +25,7 @@ local Pi = math.pi;
 
 -- also i'm not done adding all the functions yet sorry
 
+-- IMPORTANT MANAGEMENT FUNCTIONS
 local function VecToTable(vector)
 	return {vector.X, vector.Y};
 end
@@ -33,6 +38,7 @@ local function VecCopy(TVector)
 	return {TVector[1], TVector[2]};
 end
 
+-- ARITHMETIC FUNCTIONS - These change the (first) table!
 local function VecAdd(TVector1, TVector2)
 	TVector1[1] = TVector1[1] + TVector2[1];
 	TVector1[2] = TVector1[2] + TVector2[2];
@@ -57,11 +63,65 @@ local function VecDivide(TVector, divisor)
 	return TVector;
 end
 
+-- GETTERS - These don't change the input table.
+local function VecGetRoundedX(TVector)
+	return Floor(TVector[1] + 0.5)
+end
+
+local function VecGetRoundedY(TVector)
+	return Floor(TVector[2] + 0.5)
+end
+
+local function VecGetRounded(TVector)
+	return {Floor(TVector[1] + 0.5), Floor(TVector[2] + 0.5)}
+end
+
+local function VecGetFlooredX(TVector)
+	return Floor(TVector[1])
+end
+
+local function VecGetFlooredY(TVector)
+	return Floor(TVector[2])
+end
+
+local function VecGetFloored(TVector)
+	return {Floor(TVector[1]), Floor(TVector[2])}
+end
+
+local function VecGetCeilingedX(TVector)
+	return Ceil(TVector[1])
+end
+
+local function VecGetCeilingedY(TVector)
+	return Ceil(TVector[2])
+end
+
+local function VecGetCeilinged(TVector)
+	return {Ceil(TVector[1]), Ceil(TVector[2])}
+end
+
 local function VecGetMagnitude(TVector)
 	return Sqrt(TVector[1] * TVector[1] + TVector[2] * TVector[2]);
 end
-local function VecGetMagnitudeSqr(TVector)
+local function VecGetSqrMagnitude(TVector)
 	return TVector[1] * TVector[1] + TVector[2] * TVector[2];
+end
+
+local function VecGetLargest(TVector)
+	return Max(Abs(TVector[1]), Abs(TVector[2]))
+end
+
+local function VecGetSmallest(TVector)
+	return Min(Abs(TVector[1]), Abs(TVector[2]))
+end
+
+local function VecGetNormalized(TVector)
+	local returnVector = VecCopy(TVector);
+	return VecDivide(returnVector, VecGetMagnitude(TVector));
+end
+
+local function VecGetPerpendicular(TVector)
+	return {TVector[2], -TVector[1]}
 end
 
 local function VecGetAbsRadAngle(TVector)
@@ -73,6 +133,27 @@ local function VecGetAbsRadAngle(TVector)
 	end
 end
 
+local function VecGetAbsDegAngle(TVector)
+	return Deg(VecGetAbsRadAngle(TVector))
+end
+
+local function VecGetXFlipped(TVector, xFlip)
+	if xFlip then
+		return {-TVector[1], TVector[2]}
+	else
+		return TVector
+	end
+end
+
+local function VecGetYFlipped(TVector, yFlip)
+	if yFlip then
+		return {TVector[1], -TVector[2]}
+	else
+		return TVector
+	end
+end
+
+-- SETTERS - By definition these must change the table. :V
 local function VecSetMagnitude(TVector, magnitude)
 	if (TVector[1] ~= 0 or TVector[2] ~= 0) then
 		local multiplier = magnitude/VecGetMagnitude(TVector);
@@ -100,3 +181,30 @@ end
 local function VecAbsRotateTo(TVector, refVector)
 	return VecGetRadRotatedCopy(TVector, VecGetAbsRadAngle(refVector) - VecGetAbsRadAngle(TVector)); 
 end
+
+local function VecMagnitudeIsGreaterThan(TVector, magnitude)
+	return VecGetSqrMagnitude(TVector) > magnitude * magnitude;
+end
+
+local function VecMagnitudeIsLessThan(TVector, magnitude)
+	return VecGetSqrMagnitude(TVector) < magnitude * magnitude;
+end
+
+-- TO ADD:
+-- VecClampMagnitude()
+-- VecFlipX()
+-- VecFlipY()
+-- VecIsZero()
+-- VecIsOpposedTo()
+-- VecDot()
+-- VecCross()
+-- VecRound()
+-- VecToHalf()
+-- VecFloor()
+-- VecCeiling()
+-- VecNormalize()
+-- VecPerpendicularize()
+-- VecReset()
+-- VecRadRotate()
+-- VecDegRotate()
+-- VecSetXY()
